0b9934f3eb737d37c31a4a8678005193ea4458b6
IevaSurvilaite <survilaite.ieva@gmail.com>
2025-12-13 09:23:02 +0200

    deleted unused exceptions

--- Exceptions/EntityNotFoundException.cs
+++ Exceptions/EntityNotFoundException.cs
@@ -2,11 +2,7 @@ namespace PhotoScavengerHunt.Exceptions
 {
     public class EntityNotFoundException : Exception
     {
-        public EntityNotFoundException() { }
-
         public EntityNotFoundException(string message) : base(message) { }
-
-        public EntityNotFoundException(string message, Exception inner) : base(message, inner) { }
     }
 }
 
--- Exceptions/ValidationException.cs
+++ Exceptions/ValidationException.cs
@@ -2,11 +2,7 @@ namespace PhotoScavengerHunt.Exceptions
 {
     public class ValidationException : Exception
     {
-        public ValidationException() { }
-
         public ValidationException(string message) : base(message) { }
-
-        public ValidationException(string message, Exception inner) : base(message, inner) { }
     }
 }
 
